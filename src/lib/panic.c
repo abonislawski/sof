406f4e99a3addd48bc487e781d7ade5d2fc52136
Adrian Bonislawski <adrian.bonislawski@linux.intel.com>
2019-04-01 15:48:26 +0200

    panic: exception handling fix
    Signed-off-by: Adrian Bonislawski <adrian.bonislawski@linux.intel.com>

--- src/lib/panic.c
+++ src/lib/panic.c
@@ -28,63 +28,7 @@
  * Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
  */
 
-#include <sof/alloc.h>
-#include <sof/interrupt.h>
-#include <sof/mailbox.h>
 #include <sof/panic.h>
-#include <sof/sof.h>
-#include <sof/trace.h>
-
-#include <platform/platform.h>
-
-#include <uapi/ipc/trace.h>
-
-#include <stdint.h>
-#include <stdlib.h>
-
-void dump_panicinfo(void *addr, struct sof_ipc_panic_info *panic_info)
-{
-	if (!panic_info)
-		return;
-	rmemcpy(addr, panic_info, sizeof(struct sof_ipc_panic_info));
-	dcache_writeback_region(addr, sizeof(struct sof_ipc_panic_info));
-}
-
-/* panic and rewind stack */
-void panic_rewind(uint32_t p, uint32_t stack_rewind_frames,
-		  struct sof_ipc_panic_info *panic_info)
-{
-	void *ext_offset;
-	size_t count;
-	uint32_t oldps;
-
-	/* disable all IRQs */
-	oldps = interrupt_global_disable();
-
-	/* dump DSP core registers */
-	ext_offset = arch_dump_regs(oldps);
-
-	/* dump panic info, filename ane linenum */
-	dump_panicinfo(ext_offset, panic_info);
-	ext_offset += sizeof(struct sof_ipc_panic_info);
-
-	count = MAILBOX_EXCEPTION_SIZE -
-		(size_t)(ext_offset - mailbox_get_exception_base());
-	/* dump stack frames */
-	p = dump_stack(p, ext_offset, stack_rewind_frames, count);
-
-	/* panic - send IPC oops message to host */
-	platform_panic(p);
-
-	/* flush last trace messages */
-#if CONFIG_TRACE
-	trace_flush();
-#endif
-
-	/* and loop forever */
-	while (1)
-		;
-}
 
 void __panic(uint32_t p, char *filename, uint32_t linenum)
 {
