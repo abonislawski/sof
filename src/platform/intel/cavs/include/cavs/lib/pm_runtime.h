1ef0813cbc5be57ffa930d8fffd5f1fd107af559
Adrian Bonislawski <adrian.bonislawski@linux.intel.com>
2020-06-29 12:24:22 +0200

    tgl: iccmax use pm_runtime_en_dsp_pg power up instead of full core init
    This will will save a lot of time if quick changes are needed
    eg. in dynamic hpro-lpro switching which on TGL will also require to
    power up core 3
    
    Signed-off-by: Adrian Bonislawski <adrian.bonislawski@linux.intel.com>

--- src/platform/intel/cavs/include/cavs/lib/pm_runtime.h
+++ src/platform/intel/cavs/include/cavs/lib/pm_runtime.h
@@ -16,12 +16,17 @@
 #ifndef __CAVS_LIB_PM_RUNTIME_H__
 #define __CAVS_LIB_PM_RUNTIME_H__
 
+#define PWRD_MASK	MASK(31, 30)
+#define PWRD_BY_HPRO	BIT(31)
+#define PWRD_BY_TPLG	BIT(30)
+
 struct pm_runtime_data;
 
 /** \brief cAVS specific runtime power management data. */
 struct cavs_pm_runtime_data {
 	int dsp_d0_sref; /**< simple ref counter, accessed by core 0 only */
 	int host_dma_l1_sref; /**< ref counter for Host DMA accesses */
+	int dsp_client_bitmap[PLATFORM_CORE_COUNT];	/**< simple pwr lock */
 };
 
 #endif
