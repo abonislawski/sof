295c1e35cc9e27b706b188777151fb2ea889ec3a
Adrian Bonislawski <adrian.bonislawski@linux.intel.com>
2020-07-01 16:09:48 +0200

    tgl: align with HW recommended flow at platform init
     This patch will align with HW recommended flow while clock switching
     at platform init
    
    Signed-off-by: Adrian Bonislawski <adrian.bonislawski@linux.intel.com>

--- src/platform/intel/cavs/platform.c
+++ src/platform/intel/cavs/platform.c
@@ -294,11 +294,6 @@ int platform_boot_complete(uint32_t boot_message)
 {
 	uint32_t mb_offset = 0;
 
-#if CONFIG_TIGERLAKE && !CONFIG_CAVS_LPRO_ONLY
-	/* TGL specific HW recommended flow */
-	pm_runtime_get(PM_RUNTIME_DSP, PWRD_BY_HPRO | (PLATFORM_CORE_COUNT - 1));
-#endif
-
 	mailbox_dspbox_write(mb_offset, &ready, sizeof(ready));
 	mb_offset = mb_offset + sizeof(ready);
 
@@ -439,7 +434,9 @@ int platform_init(struct sof *sof)
 #elif CONFIG_CANNONLAKE || CONFIG_ICELAKE || CONFIG_TIGERLAKE
 
 	/* initialize PM for boot */
-
+#if CONFIG_TIGERLAKE
+	pm_runtime_get(PM_RUNTIME_DSP, PWRD_BY_HPRO | (PLATFORM_CORE_COUNT - 1));
+#endif
 	/* request configured ring oscillator and wait for status ready */
 	shim_write(SHIM_CLKCTL, shim_read(SHIM_CLKCTL) | CAVS_DEFAULT_RO);
 	while (!(shim_read(SHIM_CLKSTS) & CAVS_DEFAULT_RO))
@@ -469,6 +466,9 @@ int platform_init(struct sof *sof)
 #endif
 #endif
 
+#if CONFIG_TIGERLAKE && CONFIG_CAVS_LPRO_ONLY
+	pm_runtime_put(PM_RUNTIME_DSP, PWRD_BY_HPRO | (PLATFORM_CORE_COUNT - 1));
+#endif
 #elif CONFIG_SUECREEK
 	/* TODO: need to merge as for APL */
 	clock_set_freq(CLK_CPU(cpu_get_id()), CLK_MAX_CPU_HZ);
