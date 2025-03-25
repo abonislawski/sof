e9ca0e4ca99095757233ac4b156fb0e5235e8a69
Adrian Bonislawski <adrian.bonislawski@intel.com>
2025-03-25 09:08:36 +0100

    Revert "audio: asrc: move coefficients to DRAM"
    This reverts commit 901aeb3745dfb4809ef07a9cad987d715c2520fc.

--- src/audio/asrc/asrc_farrow.c
+++ src/audio/asrc/asrc_farrow.c
@@ -11,8 +11,6 @@
 #include <rtos/string.h>
 #include <sof/trace/trace.h>
 #include <sof/audio/format.h>
-#include <sof/lib/fast-get.h>
-#include <sof/lib/memory.h>
 #include <user/trace.h>
 #include "asrc_farrow.h"
 
@@ -554,31 +552,6 @@ enum asrc_error_code asrc_set_output_format(struct comp_dev *dev,
 	return ASRC_EC_OK;
 }
 
-static const int32_t *__get_polyphase_filter(const int32_t *filter, size_t size)
-{
-#if CONFIG_FAST_GET
-	return fast_get(filter, size);
-#else
-	return filter;
-#endif
-}
-
-#define get_polyphase_filter(f) __get_polyphase_filter(f, sizeof(f))
-
-static void put_polyphase_filter(const int32_t *filter)
-{
-#if CONFIG_FAST_GET
-	fast_put(filter);
-#endif
-}
-
-void asrc_free_polyphase_filter(struct asrc_farrow *src_obj)
-{
-	if (src_obj && src_obj->polyphase_filters) {
-		put_polyphase_filter(src_obj->polyphase_filters);
-		src_obj->polyphase_filters = NULL;
-	}
-}
 
 /*
  * FILTER FUNCTIONS
@@ -606,7 +579,7 @@ static enum asrc_error_code initialise_filter(struct comp_dev *dev,
 	/* Reset coefficients for possible exit with error. */
 	src_obj->filter_length = 0;
 	src_obj->num_filters = 0;
-	asrc_free_polyphase_filter(src_obj);
+	src_obj->polyphase_filters = NULL;
 
 	if (fs_in == 0 || fs_out == 0) {
 		/* Avoid possible divisions by zero. */
@@ -622,7 +595,7 @@ static enum asrc_error_code initialise_filter(struct comp_dev *dev,
 			c_filter_params[CR_48000TO48000].filter_length;
 		src_obj->num_filters =
 			c_filter_params[CR_48000TO48000].num_filters;
-		src_obj->polyphase_filters = get_polyphase_filter(coeff48000to48000);
+		src_obj->polyphase_filters = &coeff48000to48000[0];
 	} else if (fs_in <= fs_out) {
 		/* All upsampling use cases can share the same set of
 		 * filter coefficients.
@@ -631,7 +604,7 @@ static enum asrc_error_code initialise_filter(struct comp_dev *dev,
 			c_filter_params[CR_44100TO48000].filter_length;
 		src_obj->num_filters =
 			c_filter_params[CR_44100TO48000].num_filters;
-		src_obj->polyphase_filters = get_polyphase_filter(coeff44100to48000);
+		src_obj->polyphase_filters = &coeff44100to48000[0];
 	} else if (fs_in == 48000) {
 		switch (fs_out) {
 #if (CONFIG_ASRC_SUPPORT_CONVERSION_48000_TO_08000)
@@ -640,7 +613,7 @@ static enum asrc_error_code initialise_filter(struct comp_dev *dev,
 				c_filter_params[CR_48000TO08000].filter_length;
 			src_obj->num_filters =
 				c_filter_params[CR_48000TO08000].num_filters;
-			src_obj->polyphase_filters = get_polyphase_filter(coeff48000to08000);
+			src_obj->polyphase_filters = &coeff48000to08000[0];
 			break;
 #endif
 #if (CONFIG_ASRC_SUPPORT_CONVERSION_48000_TO_11025)
@@ -649,7 +622,7 @@ static enum asrc_error_code initialise_filter(struct comp_dev *dev,
 				c_filter_params[CR_48000TO11025].filter_length;
 			src_obj->num_filters =
 				c_filter_params[CR_48000TO11025].num_filters;
-			src_obj->polyphase_filters = get_polyphase_filter(coeff48000to11025);
+			src_obj->polyphase_filters = &coeff48000to11025[0];
 			break;
 #endif
 #if (CONFIG_ASRC_SUPPORT_CONVERSION_48000_TO_12000)
@@ -658,7 +631,7 @@ static enum asrc_error_code initialise_filter(struct comp_dev *dev,
 				c_filter_params[CR_48000TO12000].filter_length;
 			src_obj->num_filters =
 				c_filter_params[CR_48000TO12000].num_filters;
-			src_obj->polyphase_filters = get_polyphase_filter(coeff48000to12000);
+			src_obj->polyphase_filters = &coeff48000to12000[0];
 			break;
 #endif
 #if (CONFIG_ASRC_SUPPORT_CONVERSION_48000_TO_16000)
@@ -667,7 +640,7 @@ static enum asrc_error_code initialise_filter(struct comp_dev *dev,
 				c_filter_params[CR_48000TO16000].filter_length;
 			src_obj->num_filters =
 				c_filter_params[CR_48000TO16000].num_filters;
-			src_obj->polyphase_filters = get_polyphase_filter(coeff48000to16000);
+			src_obj->polyphase_filters = &coeff48000to16000[0];
 			break;
 #endif
 #if (CONFIG_ASRC_SUPPORT_CONVERSION_48000_TO_22050)
@@ -676,7 +649,7 @@ static enum asrc_error_code initialise_filter(struct comp_dev *dev,
 				c_filter_params[CR_48000TO22050].filter_length;
 			src_obj->num_filters =
 				c_filter_params[CR_48000TO22050].num_filters;
-			src_obj->polyphase_filters = get_polyphase_filter(coeff48000to22050);
+			src_obj->polyphase_filters = &coeff48000to22050[0];
 			break;
 #endif
 #if (CONFIG_ASRC_SUPPORT_CONVERSION_48000_TO_24000)
@@ -685,7 +658,7 @@ static enum asrc_error_code initialise_filter(struct comp_dev *dev,
 				c_filter_params[CR_48000TO24000].filter_length;
 			src_obj->num_filters =
 				c_filter_params[CR_48000TO24000].num_filters;
-			src_obj->polyphase_filters = get_polyphase_filter(coeff48000to24000);
+			src_obj->polyphase_filters = &coeff48000to24000[0];
 			break;
 #endif
 #if (CONFIG_ASRC_SUPPORT_CONVERSION_48000_TO_32000)
@@ -694,7 +667,7 @@ static enum asrc_error_code initialise_filter(struct comp_dev *dev,
 				c_filter_params[CR_48000TO32000].filter_length;
 			src_obj->num_filters =
 				c_filter_params[CR_48000TO32000].num_filters;
-			src_obj->polyphase_filters = get_polyphase_filter(coeff48000to32000);
+			src_obj->polyphase_filters = &coeff48000to32000[0];
 			break;
 #endif
 #if (CONFIG_ASRC_SUPPORT_CONVERSION_48000_TO_44100)
@@ -703,7 +676,7 @@ static enum asrc_error_code initialise_filter(struct comp_dev *dev,
 				c_filter_params[CR_48000TO44100].filter_length;
 			src_obj->num_filters =
 				c_filter_params[CR_48000TO44100].num_filters;
-			src_obj->polyphase_filters = get_polyphase_filter(coeff48000to44100);
+			src_obj->polyphase_filters = &coeff48000to44100[0];
 			break;
 #endif
 		default:
@@ -719,7 +692,7 @@ static enum asrc_error_code initialise_filter(struct comp_dev *dev,
 				c_filter_params[CR_24000TO08000].filter_length;
 			src_obj->num_filters =
 				c_filter_params[CR_24000TO08000].num_filters;
-			src_obj->polyphase_filters = get_polyphase_filter(coeff24000to08000);
+			src_obj->polyphase_filters = &coeff24000to08000[0];
 			break;
 #endif
 #if (CONFIG_ASRC_SUPPORT_CONVERSION_24000_TO_16000)
@@ -728,7 +701,7 @@ static enum asrc_error_code initialise_filter(struct comp_dev *dev,
 				c_filter_params[CR_24000TO16000].filter_length;
 			src_obj->num_filters =
 				c_filter_params[CR_24000TO16000].num_filters;
-			src_obj->polyphase_filters = get_polyphase_filter(coeff24000to16000);
+			src_obj->polyphase_filters = &coeff24000to16000[0];
 			break;
 #endif
 		default:
